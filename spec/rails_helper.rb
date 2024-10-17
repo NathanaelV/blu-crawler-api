5a2f885e944ef198aa5ebc15eb0769532be44d7d
Nathanael Vieira <tmntsplinter04@gmail.com>
2024-10-16 21:53:09 -0300

    RSpec initialization

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,6 +33,10 @@ rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
 RSpec.configure do |config|
+  config.before(type: :system) do
+		driven_by(:rack_test)
+	end
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_paths = [
     Rails.root.join('spec/fixtures')
@@ -64,5 +68,5 @@ RSpec.configure do |config|
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
-  # config.filter_gems_from_backtrace("gem name")
+  config.filter_gems_from_backtrace('ruby')
 end
