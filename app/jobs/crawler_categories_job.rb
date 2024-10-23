7bc36e137f345992dce4662697e015c7b4cfa9e8
Nathanael Vieira <tmntsplinter04@gmail.com>
2024-10-22 21:06:16 -0300

    Change CrawlerCategoriesJob to API request

--- app/jobs/crawler_categories_job.rb
+++ app/jobs/crawler_categories_job.rb
@@ -2,38 +2,14 @@ class CrawlerCategoriesJob < ApplicationJob
   queue_as :default
 
   def perform()
-    options = Selenium::WebDriver::Chrome::Options.new(
-      args: [
-        'headless',
-        'start-maximized',
-        'disable-gpu',
-        'no-sandbox',
-        'disable-setuid-sandbox',
-        'disable-dable-dev-shm-usage'
-      ]
-    )
+    response = Faraday.get('https://repnota1000.blu.com.br/api/v1/departments')
+    departments = JSON.parse(response.body)['departments']
+    departments.each do |department|
+      slug = department['slug']
+      category = Category.find_or_create_by(name: department['name'])
+      category.update(slug: slug)
 
-    driver = Selenium::WebDriver.for :chrome, options: options
-    wait = Selenium::WebDriver::Wait.new(timeout: 10)
-
-    driver.get "https://fornecedores.blu.com.br/"
-
-    wait.until { driver.find_elements(tag_name: 'li').count > 2 }
-  
-    categories_text = driver.find_elements(tag_name: 'li').map {|e| e.text }
-    categories_text.shift
-    
-    categories_text.each do |categorie_text|
-      driver.find_element(link_text: categorie_text).click
-
-      category_url = driver.current_url
-      driver.navigate.back
-
-      Category.find_or_create_by(name: categorie_text)
-
-      CollectSuppliersUrlsJob.perform_later(category_url)
+      CrawlerSuppliersJob.perform_later(slug)
     end
-
-    driver.quit
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_21_125845) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_22_235954) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
   end
 
   create_table "solid_queue_blocked_executions", force: :cascade do |t|
@@ -139,6 +140,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_21_125845) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "url"
+    t.string "cnpj"
+    t.string "slug"
     t.index ["category_id"], name: "index_suppliers_on_category_id"
   end
 
